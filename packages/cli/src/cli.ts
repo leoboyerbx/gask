bbbda74ea87bf3818b1d4f44ae3e900a77cf8d3d
leoboyerbx <contact@leoboyer.dev>
2025-12-11 11:37:04 +0100

    feat: version printing util

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { defineCommand, runMain } from 'citty'
+import { defineCommand, runMain, showUsage } from 'citty'
+import { consola } from 'consola'
 import { version } from '../package.json'
 import { buildCommand } from './commands/build'
 import { devCommand } from './commands/dev'
@@ -13,6 +14,20 @@ const main = defineCommand({
     subCommands: {
         build: buildCommand,
         dev: devCommand,
+    },
+    args: {
+        version: {
+            type: 'boolean',
+            alias: 'v',
+            description: 'Show version number',
+        },
+    },
+    async run({ args }) {
+        if (args.version !== undefined) {
+            consola.info(`Gask CLI v${version}`)
+            return
+        }
+        await showUsage(main)
     },
 })
 
