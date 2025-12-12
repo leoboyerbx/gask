9a0ad1d71c198b3714337a286598f4a23718aa08
leoboyerbx <contact@leoboyer.dev>
2025-12-12 10:16:28 +0100

    feat: frednlier error messages

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,4 +1,6 @@
+import process from 'node:process'
 import { defineCommand } from 'citty'
+import consola from 'consola'
 import { buildProject } from '../lib/build'
 import { claspPushIfNeeded } from '../lib/clasp/push'
 import { loadGaskConfig } from '../lib/config'
@@ -12,7 +14,14 @@ export const buildCommand = defineCommand({
     args: buildArgs,
     async run({ args }) {
         const { config } = await loadGaskConfig()
-        await buildProject(config)
-        await claspPushIfNeeded(config, args)
+        try {
+            await buildProject(config)
+            await claspPushIfNeeded(config, args)
+        }
+        catch (e: any) {
+            const errorMessage = e.errors?.[0].text || e.message || 'An unknown error occurred during the build process.'
+            consola.error(errorMessage)
+            process.exit(1)
+        }
     },
 })
--- packages/cli/src/lib/esbuild/copyManifest.ts
+++ packages/cli/src/lib/esbuild/copyManifest.ts
@@ -1,13 +1,20 @@
 import type { Plugin } from 'esbuild'
-import { copyFile } from 'node:fs/promises'
+import { access, copyFile } from 'node:fs/promises'
 import { getEsbuildInOut } from './_utils'
 
-export function esbuildCopyManifest(sourceManifestPath: string) {
+export function esbuildCopyManifest(sourceManifestPath: string, throwIfMissing = true) {
     return {
         name: 'gask-copy-manifest',
         setup(build) {
             const { outDir } = getEsbuildInOut(build)
             build.onEnd(async () => {
+                const exists = await access(sourceManifestPath).then(() => true).catch(() => false)
+                if (!exists) {
+                    if (throwIfMissing) {
+                        throw new Error(`Manifest file not found at path: ${sourceManifestPath}`)
+                    }
+                    return
+                }
                 await copyFile(sourceManifestPath, `${outDir}/appsscript.json`)
             })
         },
